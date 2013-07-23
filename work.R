77e4de9234dd656caf2d7596d888d7e365e5b659
Cesar <cdtorres@uw.edu>
2013-07-23 13:15:32 -0700

    updated to-do list to reflect that I added the delta parameter, edited some comments in work.R

--- work.R
+++ work.R
@@ -1,9 +1,9 @@
-#simulatetrials(theta_a = .2, theta_b = .2, prior = .2, B = 500, how_often = 1) takes 25 minutes
-#2/3 of the trials here didn't stop early
+#simulatetrials(theta_a = .2, theta_b = .2, prior = .2, B = 1000, how_often = 1, delta = .2)
+#took almost 12 minutes. 946/1000 trials stopped early.
 
 set.seed(619)
 a = Sys.time()
-x = simulatetrials(theta_a = .2, theta_b = .2, prior = .2, B = 20, how_often = 1, delta = .4)
+x = simulatetrials(theta_a = .2, theta_b = .2, prior = .2, B = 1000, how_often = 1, delta = .2)
 b = Sys.time()
 b - a
 #dat = as.data.frame(matrix(unlist(x), ncol=7, byrow=TRUE))
