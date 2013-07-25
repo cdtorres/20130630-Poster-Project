a82a87cc6bbb5208f368c06e8e90d538ddebace3
Cesar <cdtorres@uw.edu>
2013-07-25 16:45:33 -0700

    I think almost everything in the continuous case is good to go, except for the randomization probability.

--- simulatetrials.R
+++ simulatetrials.R
@@ -15,10 +15,15 @@
 # (making integral_tolerance small greatly increases the time that a simulation takes to run)
 # how_often is the number of patients we treat before evaluating P(theta_a + delta < theta_b | data),
 # after the initial 10 patients and initial evaluation
-clinicaltrial <-function(seed, theta_a, theta_b, prior, second_parameter, N, efficacy_threshold,
-                         futility_threshold, integral_tolerance, how_often, delta)
+clinicaltrial <-function(seed, theta_a, theta_b, var_a, var_b, prior, second_parameter, N,
+                         efficacy_threshold, futility_threshold, integral_tolerance, how_often, delta,
+                         type)
 {
-  
+  if(type == 'binary')
+  {
+    var_a = NULL
+    var_b = NULL
+  }
   efficacious = F
   futile = F
   stopped_early = F#indicator for if the trial stopped before all N patients were treated
@@ -26,10 +31,10 @@ clinicaltrial <-function(seed, theta_a, theta_b, prior, second_parameter, N, eff
   #This is a vector of values that we're interested in. The seven values in order are:
   #1 number of patients treated so far
   #2 probability of being assigned to treatment group a (the placebo group)
-  #3 current value of a_1, given the prior and the data obtained thus far
-  #4 current value of a_2, given the prior and the data obtained thus far
-  #5 current value of b_1, given the prior and the data obtained thus far
-  #6 current value of b_2, given the prior and the data obtained thus far
+  #3 current value of a_1, given the prior and the data obtained thus far (m_a for continuous)
+  #4 current value of a_2, given the prior and the data obtained thus far (v_a for continuous)
+  #5 current value of b_1, given the prior and the data obtained thus far (m_b for continuous)
+  #6 current value of b_2, given the prior and the data obtained thus far (v_b for continuous)
   #7 P(theta_a + delta < theta_b | data)
   v = rep(NA, 7)
   
@@ -45,11 +50,19 @@ clinicaltrial <-function(seed, theta_a, theta_b, prior, second_parameter, N, eff
   #theta_a and theta_b are the "true" probabilities of recovering with placebo and treatment, respectively
   #under the null hypothesis, they're equal
   
-  v[4] = second_parameter#initialization of a_2, given priors
-  v[6] = second_parameter#initialization of b_2, given priors
+  v[4] = second_parameter#initialization of a_2 (v_a), given priors
+  v[6] = second_parameter#initialization of b_2 (v_b), given priors
   
-  v[3] = v[4]*prior/(1 - prior)#initialization of a_1, given priors
-  v[5] = v[6]*prior/(1 - prior)#initialization of b_1, given priors
+  if(type == 'binary')
+  {
+    v[3] = v[4]*prior/(1 - prior)#initialization of a_1, given priors
+    v[5] = v[6]*prior/(1 - prior)#initialization of b_1, given priors
+  }
+  else if(type == 'continuous')
+  {
+    v[3] = prior
+    v[5] = prior
+  }
   
   #initial randomization probability, set to 1/2
   #probability of being assigned to treatment group a (the control group)
@@ -60,9 +73,11 @@ clinicaltrial <-function(seed, theta_a, theta_b, prior, second_parameter, N, eff
   #treat 10 patients before updating the randomization probability
   #or evaluating P(theta_a + delta < theta_b | data)
   if(10 <= N)
-    v = update.evaluate(theta_a, theta_b, delta, integral_tolerance, how_many = 10, ov = v)
+    v = update.evaluate(theta_a = theta_a, theta_b = theta_b, var_a = var_a, var_b = var_b, delta = delta,
+                        integral_tolerance = integral_tolerance, how_many = 10, ov = v, type = type)
   else#you can't treat more than the maximum amount of patients!
-    v = update.evaluate(theta_a, theta_b, delta, integral_tolerance, how_many = N, ov = v)
+    v = update.evaluate(theta_a = theta_a, theta_b = theta_b, var_a = var_a, var_b = var_b, delta = delta,
+                        integral_tolerance = integral_tolerance, how_many = N, ov = v, type = type)
   
   #remember, v[7] is P(theta_a + delta < theta_b | data)
   efficacious = (v[7] > efficacy_threshold)
@@ -72,9 +87,13 @@ clinicaltrial <-function(seed, theta_a, theta_b, prior, second_parameter, N, eff
   while(v[1] < N & !efficacious & !futile)
   {    
     if(v[1] + how_often <= N)
-      v = update.evaluate(theta_a, theta_b, delta, integral_tolerance, how_many = how_often, ov = v)
+      v = update.evaluate(theta_a = theta_a, theta_b = theta_b, var_a = var_a, var_b = var_b,
+                          delta = delta, integral_tolerance = integral_tolerance, how_many = how_often,
+                          ov = v, type = type)
     else#you can't treat more than the maximum amount of patients!
-      v = update.evaluate(theta_a, theta_b, delta, integral_tolerance, how_many = (N - v[1]), ov = v)
+      v = update.evaluate(theta_a = theta_a, theta_b = theta_b, var_a = var_a, var_b = var_b,
+                          delta = delta, integral_tolerance = integral_tolerance, how_many = (N - v[1]),
+                          ov = v, type = type)
     
     efficacious = (v[7] > efficacy_threshold)
     futile      = (v[7] < futility_threshold)
@@ -95,16 +114,28 @@ clinicaltrial <-function(seed, theta_a, theta_b, prior, second_parameter, N, eff
 
 # simulatetrials() simulates B clinical trials
 # For now, I use only 3 cores because my laptop isn't _that_ powerful.
-simulatetrials <- function(theta_a, theta_b, prior, B, delta, second_parameter = 5, N = 100,
-                           efficacy_threshold = 0.95, futility_threshold = 0.05,
-                           integral_tolerance = 1e-5, how_often = 5, cores = 3)
+simulatetrials <- function(theta_a, theta_b, var_a = NULL, var_b = NULL, prior, B, delta, type,
+                           second_parameter = 5, N = 100, efficacy_threshold = 0.95,
+                           futility_threshold = 0.05, integral_tolerance = 1e-5, how_often = 5, cores = 3)
 {
+  if(!(type %in% c('binary', 'continuous')))
+    stop("Please put in 'binary' or 'continuous' for the variable 'type'.")
   require(multicore)#required for mclapply()
   seeds = sample(1:(B*10), B)#for the purposes of replication
   cat("The seed is set.\n")
   #apply the clinicaltrial() function to 'seeds', passing in all the other necessary variables
-  dat = mclapply(seeds, clinicaltrial, theta_a, theta_b, prior, second_parameter, N, efficacy_threshold,
-                 futility_threshold, integral_tolerance, how_often, delta, mc.cores = cores)
+  if(type == 'binary')
+  {
+    dat = mclapply(seeds, clinicaltrial, theta_a, theta_b, NULL, NULL, prior, second_parameter, N,
+                   efficacy_threshold, futility_threshold, integral_tolerance, how_often, delta, type,
+                   mc.cores = cores)
+  }
+  else if (type == 'continuous')
+  {
+    dat = mclapply(seeds, clinicaltrial, theta_a, theta_b, var_a, var_b, prior, second_parameter, N,
+                   efficacy_threshold, futility_threshold, integral_tolerance, how_often, delta, type,
+                   mc.cores = cores)
+  }
   cat("mclapply successfully excecuted.\n")
   #format the data into something I am more comfortable with
   x = as.data.frame(matrix(unlist(dat), ncol=7, byrow=TRUE))
@@ -118,32 +149,58 @@ simulatetrials <- function(theta_a, theta_b, prior, B, delta, second_parameter =
 # is evaluated).
 # pass in the true theta_a and theta_b, delta, integral tolerance,
 # the number of patients before next evaluation, and old values
-update.evaluate <- function(theta_a, theta_b, delta, integral_tolerance, how_many, ov)
+update.evaluate <- function(theta_a, theta_b, var_a, var_b, delta, integral_tolerance,
+                            how_many, ov, type)
 {
   #require(pracma)#required for dblquad()
   nv = rep(NA, length(ov))#the updated values that will be returned by this function (new values)
   
-  #simulate the random assignments
+  #simulate the random assignments (this is the same regardless of outcome type)
   assigned_to_a = rbinom(1, how_many, ov[2])
   assigned_to_b = how_many - assigned_to_a
   nv[1] = ov[1] + how_many
   
-  #of those assigned to treatment a, simulate the number of "successes" (recovery)
-  sum_a = rbinom(1, assigned_to_a, theta_a)
-  sum_b = rbinom(1, assigned_to_b, theta_b)
+  if(type == 'binary')
+  {
+    #of those assigned to treatment a, simulate the number of "successes" (recovery)
+    sum_a = rbinom(1, assigned_to_a, theta_a)
+    sum_b = rbinom(1, assigned_to_b, theta_b)
+  }
+  else if(type == 'continuous')
+  {
+    sum_a = sum(rnorm(assigned_to_a, theta_a, var_a))
+    sum_b = sum(rnorm(assigned_to_b, theta_b, var_b))
+  }
   
-  #update parameters a_1, a_2, b_1, b_2
-  nv[3] = ov[3] + sum_a
-  nv[4] = ov[4] + assigned_to_a - sum_a
-  nv[5] = ov[5] + sum_b
-  nv[6] = ov[6] + assigned_to_b - sum_b
+  if(type == 'binary')
+  {
+    #update parameters a_1, a_2, b_1, b_2
+    nv[3] = ov[3] + sum_a
+    nv[4] = ov[4] + assigned_to_a - sum_a
+    nv[5] = ov[5] + sum_b
+    nv[6] = ov[6] + assigned_to_b - sum_b
+  }
+  else if(type == 'continuous')
+  {
+    #update parameters m_a, v_a, m_b, v_b
+    nv[3] = (ov[3]/ov[4] + sum_a/var_a) / (1/ov[4] + assigned_to_a/var_a)
+    nv[4] = (1/ov[4] + assigned_to_a/var_a)^-1
+    nv[5] = (ov[5]/ov[6] + sum_b/var_b) / (1/ov[6] + assigned_to_b/var_b)
+    nv[6] = (1/ov[6] + assigned_to_b/var_b)^-1
+  }
   
-  #estimate the treatment effects
-  theta_a_hat = nv[3]/(nv[3] + nv[4])
-  theta_b_hat = nv[5]/(nv[5] + nv[6])
-  
-  #update the probability of being assigned to treatment group a (the control group)
-  nv[2] = theta_a_hat/(theta_a_hat + theta_b_hat)
+  if(type == 'binary')
+  {
+    #estimate the treatment effects
+    theta_a_hat = nv[3]/(nv[3] + nv[4])
+    theta_b_hat = nv[5]/(nv[5] + nv[6])
+  }
+  else if(type == 'continuous')
+  {
+    #estimate the treatment effects
+    theta_a_hat = nv[3]
+    theta_b_hat = nv[5]
+  }
   
   #calculate the probability that theta_a + delta < theta_b, given our current distributions for them
   #integrand is joint density for theta_a and theta_b, defined on theta_a + delta < theta_b
@@ -154,13 +211,48 @@ update.evaluate <- function(theta_a, theta_b, delta, integral_tolerance, how_man
   
   #nv[7] = dblquad(integrand, 0, 1, 0, 1, tol = integral_tolerance)
   
-  integrand = function(y)
+  if(type == 'binary')
   {
-    pbeta((y - delta), nv[3], nv[4])*dbeta(y, nv[5], nv[6])
+    integrand = function(y)
+    {
+      pbeta((y - delta), nv[3], nv[4])*dbeta(y, nv[5], nv[6])
+    }
+  }
+  else if(type == 'continuous')
+  {
+    integrand = function(y)
+    {
+      pnorm((y - delta), nv[3], nv[4])*dnorm(y, nv[5], nv[6])
+    }
   }
   
-  nv[7] = integrate(integrand, 0, 1)$value
+  if(type == 'binary')
+  {
+    nv[7] = integrate(integrand, 0, 1)$value
+  }
+  else if(type == 'continuous')
+  {
+    nv[7] = integrate(integrand, -Inf, Inf)$value
+  }
   
+  #update the probability of being assigned to treatment group a (the control group)
+  if(type == 'binary')
+    nv[2] = theta_a_hat/(theta_a_hat + theta_b_hat)
+  else if(type == 'continuous')
+  {
+#     #going off of page 156
+#     r_integrand = function(y)#randomization integrand
+#     {
+#       pnorm(y, nv[3], nv[4])*dnorm(y, nv[5], nv[6])
+#     }
+#     r_integral = integrate(r_integrand, -Inf, Inf)$value#randomization integral
+#     c = nv[1]/(2*100)
+#     r_1 = (1 - r_integral)^c
+#     r_2 = (r_integral)^c
+#     nv[2] = r_1/(r_1 + r_2)
+#     global_probability <<- r_integral
+    nv[2] = .2
+  }
   return(nv)
 }
 
--- work.R
+++ work.R
@@ -1,23 +1,39 @@
-#x = simulatetrials(theta_a = .2, theta_b = .2, prior = .2, B = 1000, how_often = 1, delta = .2)
-#took 12 minutes 22 seconds. 946/1000 trials stopped early.
-
+# true arm effects
 theta_a = 0.2
 theta_b = 0.2
-#note: the Beta distribution with mean 0.5 and second parameter 1 is in fact the Uniform(0,1) distribution
-prior = 0.5
-second_parameter = 1
-B = 10000
+var_a   = 1
+var_b   = 1
+type = 'continuous'
+##################################################################
+# note: the Beta distribution with mean 0.5 and second parameter 1
+# is in fact the Uniform(0,1) distribution
+if(type == 'continuous')
+{
+  prior = 0
+  second_parameter = 1
+}
+if(type != 'continuous')
+{
+  prior = 0.5
+  second_parameter = 1
+}
+##################################################################
+# number of simulations
+B = 40
+# after the initial 10 patients, how often do we want to update the posterior distributions?
 how_often = 1
-delta = 0.2
+delta = 1
+
+
 a = Sys.time()#to time my code
 set.seed(619)#for the purposes of replication
-x = simulatetrials(theta_a = .2, theta_b = .2, prior = .2, B = 1000, how_often = 1, delta = .2)
-#x = simulatetrials(theta_a = theta_a, theta_b = theta_b, prior = prior,
-#                   second_parameter = second_parameter, B = B, how_often = how_often, delta = delta)
+x = simulatetrials(theta_a = theta_a, theta_b = theta_b, var_a = var_a, var_b = var_b, prior = prior,
+                   second_parameter = second_parameter, B = B, how_often = how_often, delta = delta,
+                   type = type)
 b = Sys.time()#to time my code
 b - a#amount of time my code took to run
 
 
-dim(unique(as.matrix(x)))[1]#number of unique simulations
-table(x[,6])#table of the number of patients used in the clinical trials
-simsum(x)
+#dim(unique(as.matrix(x)))[1]#number of unique simulations
+#table(x[,6])#table of the number of patients used in the clinical trials
+#simsum(x)
